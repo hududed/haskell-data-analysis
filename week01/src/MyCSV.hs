f778399385ae7c8bf2e9516bce505cc9f0ac1775
Hud <hudwahab@gmail.com>
2021-12-27 08:10:35 -0700

    example helpers

--- week01/src/MyCSV.hs
+++ week01/src/MyCSV.hs
@@ -8,7 +8,9 @@ noEmptyRows = either (const []) (filter (\row -> 2 <= length row))
 {- Reads a column of data from a CSV file -}
 readIndex :: Read cell => Either a CSV -> Int -> [cell]
 readIndex ecsv index = map read (getIndex ecsv index)
+-- e.g. awayRuns = readIndex testCSV ColNum :: [Integer]
 
 {- Reads a column of data from a CSV file -}
 getIndex :: Either a CSV -> Int -> [String]
 getIndex ecsv index = map (!! index) (noEmptyRows ecsv)
+-- e.g. gameDates = getIndex testCSV ColNum
